2269811dcb9200e0a173a09f656e2adac999e3e3
Vladimir Novichikhin <darkonica@gmail.com>
2025-02-23 21:23:36 +0300

    shawarma + click functional

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,7 +4,8 @@ export class Game extends Scene
 {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+    counterView: Phaser.GameObjects.Text;
+    shawarma: Phaser.GameObjects.Image; // TODO: crop png later
 
     constructor ()
     {
@@ -13,23 +14,49 @@ export class Game extends Scene
 
     create ()
     {
+        let counter = 0;
+
+
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+        // this.background = this.add.image(512, 384, 'background');
+        // this.background.setAlpha(0.5);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
+        this.counterView = this.add.text(300, 100, String(counter), {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         });
-        this.msg_text.setOrigin(0.5);
+        this.counterView.setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
+        this.shawarma = this.add.image(300, 400, 'shawarma');
+        this.shawarma.setScale(0.5);
+        this.shawarma
+            .setInteractive()
+            .on('pointerdown', () => {
+                counter++;
+                this.counterView.setText(String(counter));
+                
+                this.tweens.add({
+                    targets: this.shawarma,
+                    scaleX: 0.48,
+                    scaleY: 0.48,
+                    duration: 50,
+                    yoyo: true
+                });
+            })
+            .on('pointerover', () => {
+                this.input.setDefaultCursor('pointer');
+            })
+            .on('pointerout', () => {
+                this.input.setDefaultCursor('default');
+            });
 
-            this.scene.start('GameOver');
+        // this.input.once('pointerdown', () => {
 
-        });
+        //     this.scene.start('GameOver');
+
+        // });
     }
 }
